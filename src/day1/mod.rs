a60bf482aeb42c388bd40cf247a5c08967cae426
cyril delmas <cyril.delmas@gmail.com>
2022-12-02 22:57:22 +0100

    refactor: use itertools trait

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,5 +1,5 @@
-use anyhow::{anyhow, Result};
-use itertools::{rev, sorted};
+use anyhow::Result;
+use itertools::Itertools; //{rev, sorted};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
@@ -7,14 +7,14 @@ use std::path::PathBuf;
 pub fn calories_carried(input: &PathBuf) -> Result<u32> {
     let file = File::open(input)?;
     let reader = BufReader::new(file);
-    Ok(rev(sorted(
-        reader
-            .lines()
-            .filter_map(std::io::Result::ok)
-            .collect::<Vec<String>>()
-            .split(|s| s.is_empty())
-            .map(|sl| sl.iter().filter_map(|e| e.parse::<u32>().ok()).sum::<u32>()),
-    ))
-    .take(3)
-    .sum::<u32>())
+    Ok(reader
+        .lines()
+        .filter_map(std::io::Result::ok)
+        .collect::<Vec<String>>()
+        .split(|s| s.is_empty())
+        .map(|sl| sl.iter().filter_map(|e| e.parse::<u32>().ok()).sum::<u32>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<u32>())
 }
